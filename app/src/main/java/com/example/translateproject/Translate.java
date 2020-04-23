3de784c653e59e0cbc3713083ee4032b9f95fdfd
Aleyna Karabacakoğlu <50250045+AleynaKarabacakoglu@users.noreply.github.com>
2020-04-23 18:46:50 +0300

    updated

--- app/src/main/java/com/example/translateproject/Translate.java
+++ app/src/main/java/com/example/translateproject/Translate.java
@@ -2,7 +2,7 @@ package com.example.translateproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.AsyncTask;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -14,44 +14,28 @@ import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-
+import com.google.firebase.auth.FirebaseAuth;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLConnection;
-import java.net.URLEncoder;
-
-import javax.net.ssl.HttpsURLConnection;
 
 
 public class Translate extends AppCompatActivity {
     private static final String TAG = "Translate";
     private EditText etInput;
     private Spinner spLang,spLang2;
-    private String[] language ={"az","en","tr"};
-    private String[] language2 ={"az","en","tr"};
+    private String[] language ={"az","en","tr"}; //burada spinner kullanmayı göstermek amacıyla 3 dil ekledim, bunu çoğaltabiliriz.
+    private String[] language2 ={"az","en","tr"};//az-azerice tr-türkçe en - ingilizce çeviri için
     public static TextView txtOutput;
-    private Button translateBtn;
+    private Button translateBtn,exitBtn;
     private ImageButton oppositeBtn;
-    private String word, lang;
+    private String word;
     private RequestQueue mqueue;
     private ArrayAdapter<String> dataAdaptorforPlainText;
     private ArrayAdapter<String> dataAdaptorforTranslationText;
@@ -66,6 +50,7 @@ public class Translate extends AppCompatActivity {
         spLang=findViewById(R.id.spinnerLang);
         spLang2=findViewById(R.id.spinnerLang2);
         mqueue = Volley.newRequestQueue(this);
+        exitBtn=findViewById(R.id.btnExit);
 
     }
 
@@ -117,20 +102,34 @@ public class Translate extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 word=etInput.getText().toString();
-                //lang="tr-en";
+
                 String dil=spLang.getSelectedItem().toString()+"-"+spLang2.getSelectedItem().toString();
-
+                //Spinnerdan aldığım dilleri JsonParse metoduma parametre olarak verdim.
                 JsonParse(word,dil);
-
-
-
             }
+        });
+
+        exitBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+
+                Intent i = new Intent(Translate.this, SignInActivity.class);
+                //Açık olan hesabımızı aşağıdaki metodu kullanarak kapatabiliyoruz.
+                FirebaseAuth.getInstance().signOut();
+                startActivity(i);
+            }
+
         });
 
     }
 
     private void JsonParse(String msg,String lang) {
         Log.d(TAG, "JsonParse: BU METOD ÇALIŞTI");
+        //Aşağıdaki url "yandexkey+çevirilecek kelime+ çivirdiğimiz dil-çevirilen dil" den oluşur.
+        //url yi tam olarak tarayıcınızda çalıştırırsanız bir Json dizisi döndüğünü görürsünüz.
+
         String url = "https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20200420T213659Z.6ffe4c582a0f4540.30079ac61936c66e67b74dde73ce33c6509972ad&text="+msg+"&lang="+lang;
         JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
             @Override
@@ -139,6 +138,11 @@ public class Translate extends AppCompatActivity {
                 JSONArray jsonArray = null;
                 try {
                     jsonArray = response.getJSONArray("text");
+                    /*json da istediğimiz sonuc "text" dizisinin içinde olduğu için diziyi parse ettim.
+                     burada kullandığım kütüphane volley kütüphanesidir, android studio da json kütüphanesinde jsonparser
+                     metodu çalışmamaktır, eğer json kütüphanesi kullanacaksanız gson implement etmeniz gerekir.
+                     Ben volley kullandığım için, build.gradleda onu implement ettim.*/
+
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
